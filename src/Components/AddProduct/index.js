a898517e5b217eb3e6af2a75c7b325d5e82d1b1f
ParyushiJain <80160215+ParyushiJain@users.noreply.github.com>
2022-06-29 22:35:28 +0530

    Add Product completed

--- src/Components/AddProduct/index.js
+++ src/Components/AddProduct/index.js
@@ -11,7 +11,7 @@ function Textfield(props) {
     <div className="col-span-6 sm:col-span-3">
       <label
         htmlFor="first-name"
-        className="block text-sm font-medium text-gray-700"
+        className="text-left block text-sm font-medium text-gray-700"
       >
         {props.Label}
       </label>
@@ -19,12 +19,33 @@ function Textfield(props) {
         type={props.Type}
         name={props.Name}
         id={props.id}
-        className="mt-1 p-2 border focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+        className="mt-1 p-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
       />
     </div>
   );
 }
-function AddProduct() {
+const SelectField = (props)=>{
+  return (
+    <div>
+    <label
+      htmlFor={props.Title}
+      className="block text-left text-sm font-medium text-gray-700"
+    >
+      {props.Title}
+    </label>
+    <select
+      id={props.Title}
+      name={props.Title}
+      className="mt-1 block w-full py-1 px-3 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+    >
+      {props.Array.map((el) => {
+        return <option value={el} key={el}>{el===true ? "true" : el===false ? "false" : el}</option>;
+      })}
+    </select>
+  </div>
+  )
+}
+ function AddProduct() {
   let sid = useSellerId();
   let headers = useToken();
   let { Categories } = useSellerData();
@@ -75,6 +96,7 @@ function AddProduct() {
       id: sid,
       Category: e.target[2].value,
       Photo: imgUrl,
+      InStock : e.target[3].value
     };
     axios.put("/api/createProduct", data,{headers : headers}).then((data) => {
       alert("Success");
@@ -85,13 +107,12 @@ function AddProduct() {
   return (
     <div>
       <form onSubmit={submit}>
-        <div className="shadow overflow-hidden ">
-          <h1 className=" text-2xl">Add Products</h1>
+          
 
-          <div className=" bg-white ">
-            <div className="flex flex-row ">
-              <div className="flex flex-col m-20">
-                <Textfield
+          <div className="">
+            <div className="flex flex-col ">
+              <div className="flex flex-row m-4 p-4 bg-WbgGrey justify-between rounded-xl">
+                <Textfield 
                 
                   Label="Product Name"
                   Type="text"
@@ -104,37 +125,46 @@ function AddProduct() {
                   Name="Price"
                   id="Price"
                 />
-                <div>
-                  <label
-                    htmlFor="Category"
-                    className="block text-sm font-medium text-gray-700"
-                  >
-                    Category
-                  </label>
-                  <select
-                    id="Category"
-                    name="Category"
-                    className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                  >
-                    {Categories.map((el) => {
-                      return <option value={el} key={el}>{el}</option>;
-                    })}
-                  </select>
-                </div>
+               <SelectField 
+                Title="Category"
+                Array={Categories}
+               />
+              <SelectField 
+                Title="InStock"
+                Array={[true,false]}
+               />
               </div>
 
-              <div className="m-auto" >
+              <div className="m-auto bg-WbgGrey p-4 rounded-xl" >
                 <div>
-                  <input
+                  {/* <input
                     type="file"
                     onChange={onChangePicture}
                     accept="image/x-png,image/gif,image/jpeg" 
-                  />
+                  /> */}
+                  <div class="m-4">
+            <label class="inline-block mb-2 text-gray-500">Upload Product 
+                Image</label>
+            <div class="flex items-center justify-center w-full">
+                <label class="flex flex-col w-full h-32 bg-white rounded-lg  hover:bg-gray-100 ">
+                    <div class="flex flex-col items-center justify-center pt-7">
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                            class="w-12 h-12 text-gray-400 group-hover:text-gray-600" viewBox="0 0 20 20"
+                            fill="currentColor">
+                            <path fill-rule="evenodd"
+                                d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z"
+                                clip-rule="evenodd" />
+                        </svg>
+                        <p class="pt-1 text-sm tracking-wider text-gray-400 group-hover:text-gray-600">
+                            {imgData ? "Select Another Photo" :  "Select a photo"}</p>
+                    </div>
+                    <input type="file" onChange={onChangePicture} accept="image/x-png,image/gif,image/jpeg"  class="opacity-0" />
+                </label>
+            </div>
+        </div>
                   {imgData === null ? (
                     <>
-                      <div>
-                        <h3 className="text-center" >Upload Product Image</h3>
-                      </div>
+                     
                     </>
                   ) : (
                     <div className="text-center">
@@ -163,12 +193,12 @@ function AddProduct() {
           <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
             <button
               type="submit"
-              className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-lightgreen hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-NavbarBg hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
               Save
             </button>
           </div>
-        </div>
+      
       </form>
     </div>
   );
